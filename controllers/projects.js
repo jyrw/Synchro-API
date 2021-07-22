9df985a31e8143e4542da4965a4a6d7ad4395bf4
Yong Ray Wen Joshua <e0544562@u.nus.edu>
2021-07-22 18:18:45 +0800

    Further modified API endpoints

--- controllers/projects.js
+++ controllers/projects.js
@@ -5,6 +5,7 @@ import Event from "../models/eventModel.js";
 
 const projectsRouter = express.Router();
 
+/* Project creation now handled in user router
 // Create a new project, return the project id in json (correct way?) // TODO check for duplicate project name
 projectsRouter.post('/', (req, res) => {
     const auth = req.currentUser;
@@ -13,26 +14,28 @@ projectsRouter.post('/', (req, res) => {
         const project = new Project(req.body);
         User.findOne({uid: uid}, (err, user) => {
             project.users.push(user);
-            console.log(user);
             project.save();
-            return res.status(201).json({savedProjectId: project._id});
+            user.projects.push(project);
+            user.save();
+            return res.status(201).send('Created new project');
         })
     } else {
         return res.status(403).send('Not authorized');
     }
 });
+*/
 
 // Given a project id, return the project document
 projectsRouter.get('/:projectId', async (req, res) => {
     const auth = req.currentUser;
     if (auth) {
         const projectId = req.params.projectId;
-        Project.findOne({_id: projectId}, (err, project) => {
-            if (err) {
-                console.log(err);
-            }
-            return res.status(200).json(project.toJSON()); // TODO: populate/calculate and/or separate into diff endpoints
-        });
+        Project.findOne({_id: projectId})
+            .populate('users', 'displayName email')
+            .populate('events')
+            .exec((err, project) => {
+                 return res.status(200).json(project); // TODO: calculate? and/or separate into diff endpoints
+            });
     } else {
         return res.status(403).send('Not authorized');
     }
@@ -46,11 +49,33 @@ projectsRouter.put('/:projectId/users', (req, res) => {
         const email = req.body.email;
         Project.findOne({_id: projectId}, (err, project) => {
             User.findOne({email: email}, (err, user) => {
+                if (!user) {
+                    return res.status(404).send('User does not exist');
+                }
                 project.users.push(user);
                 project.save();
+                user.projects.push(project);
+                user.save();
                 return res.status(200).send('User added to project');
             })
         })
+    }
+})
+
+// Creates an event with request body and adds it to the project's event array.
+projectsRouter.put('/:projectId/events', (req, res) => {
+    const auth = req.currentUser;
+    if (auth) {
+        const projectId = req.params.projectId;
+        const event = new Event(req.body);
+        Project.findOne({_id: projectId}, (err, project) => {
+            event.save();
+            project.events.push(event);
+            project.save();
+            return res.status(201).send('Event created');
+        })
+    } else {
+        return res.status(403).send('Not authorized');
     }
 })
 
